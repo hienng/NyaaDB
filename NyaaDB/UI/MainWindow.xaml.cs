1997dc8ca09c136134efe592020126da13897a08
Hien Nguyen <hien-nguyen@gmx.de>
2017-06-16 11:33:55 +0200

    make readonly settings

--- NyaaDB/UI/MainWindow.xaml.cs
+++ NyaaDB/UI/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace NyaaDB.UI
     /// </summary>
     public partial class MainWindow : Window
     {
-        private DefaultDBSettings _dbSettings;
+        private readonly DefaultDBSettings _dbSettings;
 
         public MainWindow(DefaultDBSettings aDefaultDBSettings)
         {
